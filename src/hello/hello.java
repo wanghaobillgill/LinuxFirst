eaf0840b0332f512be82193e517a7fde8d2be12b
wanghao <wanghaobillgill@hotmail.com>
2019-06-28 19:40:23 +0800

    Idea Test first

---
+++ src/hello/hello.java
@@ -0,0 +1,47 @@
+package hello;
+
+public class hello {
+	public static void main(String[] args) {
+		/*
+		 * String str = "We are student"; int a = str.length(); System.out.println(a);
+		 * int indexFirst=str.indexOf(" "); int indexLast=str.lastIndexOf(" ");
+		 * System.out.println("第一次出现的位置"+indexFirst);
+		 * System.out.println("最后一次出现的位置"+indexLast);
+		 */
+//		String str="hello Word";
+//		char mychar=str.charAt(6);
+//		String substring=str.substring(0);
+//		String substring=str.substring(3,5);
+//		System.out.println(substring);
+		/*
+		 * String haveEnter="   Hello I am Bill   "; String haveString=haveEnter.trim();
+		 * //trim()方法可以去除所有的空格（前面和后面的） System.out.println(haveString);
+		 * System.out.println(haveEnter.length());
+		 * System.out.println(haveString.length());
+		 */
+		/*
+		 * String str = "address"; String newstr = str.replace("d", "D"); //
+		 * 替换原来字符串所有的字母 System.out.println(newstr); String hiString =
+		 * str.concat("Hello"); //拼接字符串 System.out.println(hiString);
+		 */
+//		boolean b=str.startsWith("a");
+//		boolean b1=str.endsWith("a");
+//		System.out.println(b);
+//		System.out.println(b1);
+//		System.out.println(reString);
+		/*String tom = new String("I am a student");
+		String jerry = "I am a student";
+		String a = "hello2";
+		final String b="hello";		//使用b值的时候相当于直接使用字符串“Hello”
+		String d="hello";
+		String c=b+2;
+		String e=d+2;
+		boolean b1 = (a==c);
+		boolean b2=a.equals(e);
+//		boolean b1=tom.equals(jerry);
+		System.out.println(b1);
+		System.out.println(b2);*/
+
+		
+	}
+}
